3428eb27f3c44d9871dce6af220507c0b6ba21ca
David Nunez <arizonatribe@gmail.com>
2018-09-10 09:34:47 -0700

    Renamed casing helpers and added toCamelCase(), first() and last()

--- test/case.test.js
+++ test/case.test.js
@@ -1,9 +1,9 @@
 const tape = require('tape')
-const capitalize = require('../lib/capitalize')
+const toTitleCase = require('../lib/toTitleCase')
 
 tape('"capitalize" will convert the first letter of a word to uppercase', t => {
-  t.equal(capitalize('lorem'), 'Lorem')
-  t.equal(capitalize('lorem ipsum'), 'Lorem Ipsum')
-  t.equal(capitalize(' lorem ipsum dolor sit amet '), ' Lorem Ipsum Dolor Sit Amet ')
+  t.equal(toTitleCase('lorem'), 'Lorem')
+  t.equal(toTitleCase('lorem ipsum'), 'Lorem Ipsum')
+  t.equal(toTitleCase(' lorem ipsum dolor sit amet '), ' Lorem Ipsum Dolor Sit Amet ')
   t.end()
 })
