19046406fdb20cdf0624797dca6ba9b68453aca1
David Nunez <arizonatribe@gmail.com>
2018-10-18 23:52:42 -0700

    Added earlier targeting for umd build

--- src/shim.js
+++ src/shim.js
@@ -13,6 +13,19 @@ const getter = {
   }
 }
 
+/**
+ * Recursively shims an Object. Every time the getter is invoked
+ * (which happens whenever prop paths are referenced in the consuming code),
+ * the path is shimmed with another Proxy.
+ * It is not a polyfill but rather a way to keep deep prop paths that may not
+ * exist on the source object from throwing an error.
+ *
+ * @func
+ * @sig {k: v} -> Proxy
+ * @param {Object} obj An object that will be the Proxy's source
+ * @returns {Proxy} An Object that will return props on the source Object if
+ * they exist but safely handle missing prop paths without throwing errors.
+ */
 function shim(obj) {
   return new Proxy(obj, getter)
 }
--- src/size.js
+++ src/size.js
@@ -1,5 +1,21 @@
 import isUndefined from './isUndefined'
 
+/**
+ * Checks the length (or size) of many different types of values:
+ *   - Array
+ *   - Set
+ *   - Map
+ *   - Object (num of keys)
+ *   - String (num of chars)
+ *   - Function (num of params)
+ *
+ * @func
+ * @sig Array|String|Object -> Number
+ * @param {Object|String|Array|Function} val A value of type Object, String,
+ * Array or Function
+ * @returns {Number} The length of the String or Array, OR the number of keys in
+ * the Object
+ */
 function size(val) {
   if (!isUndefined(val.length)) {
     return val.length
